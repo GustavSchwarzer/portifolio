ea93fb9c813603fe911061787bcfc745c14d3c97
GustavSchwarzer <gustavschwarzer.oficial@gmail.com>
2022-08-24 15:09:05 -0300

    botão "contratar-me" e ativação do menu mobile

--- script.js
+++ script.js
@@ -10,3 +10,8 @@ $(document).ready(function(){
         
     });
 });
+
+$('.menu-btn').click(function(){
+    $('.navbar .menu').toggleClass("active");
+    $('.menu-btn i').toggleClass("active");
+});
