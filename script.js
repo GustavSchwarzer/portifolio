6c5453997f84c87517f6a9f667dba038b8489dce
GustavSchwarzer <gustavschwarzer.oficial@gmail.com>
2022-09-01 18:02:15 -0300

    Finalização do projeto

--- script.js
+++ script.js
@@ -2,16 +2,38 @@ $(document).ready(function(){
     $(window).scroll(function(){
         if(this.scrollY > 20){
             $('.navbar').addClass("sticky");
-           
+
         }else{
             $('.navbar').removeClass("sticky");
-           
         }
-        
-    });
-});
 
-$('.menu-btn').click(function(){
-    $('.navbar .menu').toggleClass("active");
-    $('.menu-btn i').toggleClass("active");
+    });
+
+
+    $('.carousel').owlCarousel({
+        margin:20,
+        loop:true,
+        autoplayTimeOut:2000,
+        autoplayHoverPauser:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:false
+            },
+            600:{
+                items:2,
+                nav:false
+            },
+            1000:{
+                items:3,
+                nav:false
+            }
+        }
+    });
+
+
 });
+ $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
